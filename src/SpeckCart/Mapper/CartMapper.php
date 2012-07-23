ac30c416d5d61d957bc87b12c79c1393d37c2d52
Ben Youngblood <bx.youngblood@gmail.com>
2012-07-23 11:37:51 -0700

    prototype cart db mapper

---
+++ src/SpeckCart/Mapper/CartMapper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SpeckCart\Mapper;
+
+use SpeckCart\Entity\Cart;
+use SpeckCart\Entity\CartInterface;
+
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Where;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+use ZfcBase\Mapper\AbstractDbMapper;
+
+class CartMapper extends AbstractDbMapper
+{
+    protected $tableName = 'cart';
+    protected $cartIdField = 'cart_id';
+
+    public function __construct()
+    {
+        $this->setEntityPrototype(new Cart);
+        $this->setHydrator(new ClassMethods(true));
+    }
+
+    public function findById($cartId)
+    {
+        $select = new Select;
+        $select->from($this->tableName);
+
+        $where = new Where;
+        $where->equalTo($this->cartIdField, $cartId);
+
+        $resultSet = $this->selectWith($select->where($where));
+        return $resultSet->current();
+    }
+
+    public function persist(CartInterface $cart)
+    {
+        if ($cart->getCartId() > 0) {
+            $where = new Where;
+            $where->equalTo($this->cartIdField, $cartId);
+
+            $this->update($cart, $where, $this->tableName);
+        } else {
+            $this->insert($cart, $this->tableName);
+        }
+    }
+}
