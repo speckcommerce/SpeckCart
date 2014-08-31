ccacdba7d16f356a06632a32315a01c4cba592cc
Aleksey Khudyakov <aleksey@xerkus.pro>
2014-08-31 21:34:11 +1100

    Reset cart for rewrite

--- config/module.config.php
+++ config/module.config.php
@@ -5,53 +5,10 @@ return array(
             'speckcart_db_adapter' => 'Zend\Db\Adapter\Adapter'
         ),
     ),
-
-    'controllers' => array(
-        'invokables' => array(
-            'speckcart' => 'SpeckCart\Controller\CartController',
-        ),
-    ),
-
-    'router' => array(
-        'routes' => array(
-            'cart' => array(
-                'type' => 'Literal',
-                'priority' => 1000,
-                'options' => array(
-                    'route' => '/cart',
-                    'defaults' => array(
-                        'controller' => 'speckcart',
-                        'action' => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'empty-cart' => array(
-                        'type' => 'Literal',
-                        'options' => array(
-                            'route'    => '/empty-cart',
-                            'defaults' => array(
-                                'controller' => 'speckcart',
-                                'action' => 'empty-cart',
-                            ),
-                        ),
-                    ),
-                    'update-quantities' => array(
-                        'type'    => 'Literal',
-                        'options' => array(
-                            'route'    => '/update-quantities',
-                            'defaults' => array(
-                                'controller' => 'speckcart',
-                                'action' => 'update-quantities',
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-
     'view_manager' => array(
+        'controller_map' => [
+            'SpeckCommerce\Cart' => true,
+        ],
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
